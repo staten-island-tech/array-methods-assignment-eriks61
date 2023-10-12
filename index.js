25db4fe9fda02756d80f45035c4de3f058893251
eriks61 <eriks61@nycstudents.net>
2023-10-12 09:23:41 -0400

    435

--- index.js
+++ index.js
@@ -3,11 +3,34 @@
 // console.log(arr[2])
 // console.log(arr[2])
 
-const arr2 = [true, false, "yay!", [6, 7, 9], null, 9];
-console.log(arr2);
-console.log(arr2.length);
+//const arr2 = [true, false, "yay!", [6, 7, 9], null, 9];
+//console.log(arr2);
+//console.log(arr2.length);
 
-arr2.push("goes at end"); //moves to front of array
-arr2.unshift("goes in front"); //moves to end of arrray
-arr2.pop(); //changes last item in array to whats in the parenthesis
-console.log(arr2);
+//arr2.push("goes at end"); //moves to front of array
+//arr2.unshift("goes in front"); //moves to end of arrray
+//arr2.pop(); //changes last item in array to whats in the parenthesis
+//console.log(arr2);
+let year = 2023;
+const students = [];
+const Travis = {
+    firstName: "Travis",
+    lastName: "Tam",
+    DOB: 2008,
+    graduated: false, 
+    age:function () {
+        return year - this.DOB;
+    }
+}
+const Oleg = {
+   firstName: "Oleg",
+   lastName: "Pony",
+   DOB: 2008,
+   graduated: false,
+   age: function () {
+    return year - this.DOB;
+   }
+}
+
+const arr = [Travis,Oleg];
+students.forEach((student) => console.log(arr[0,1]));
